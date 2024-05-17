ae96341d8363158f3d8350a006e968260400f9a6
woojin-choi518 <woojin.choi518@gmail.com>
2024-05-17 14:01:37 +0900

    add glb files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import React, { useRef, useEffect, Suspense } from 'react';  // Suspense를 여기에 추가
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import React, { useRef, useEffect, Suspense, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Environment, useGLTF } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import * as THREE from 'three';  // THREE 네임스페이스를 임포트
+import * as THREE from 'three';
 
 const Model = ({ modelPath }) => {
   const group = useRef();
   const { scene, animations } = useGLTF(modelPath);
-  const mixer = useRef(new THREE.AnimationMixer(scene));  // mixer를 여기서 초기화
+  const mixer = useRef();
 
   useEffect(() => {
-    const action = mixer.current.clipAction(animations[0]);
-    action.play();
+    if (scene) {
+      mixer.current = new THREE.AnimationMixer(scene);
+      const action = mixer.current.clipAction(animations[0]);
+      action.play();
+    }
 
-    return () => animations.forEach((clip) => mixer.current.uncacheClip(clip));
-  }, [animations]);
+    return () => {
+      if (animations.length && mixer.current) {
+        animations.forEach((clip) => mixer.current.uncacheClip(clip));
+      }
+    };
+  }, [modelPath, scene, animations]);
 
-  useFrame((state, delta) => mixer.current.update(delta));
+  useFrame((state, delta) => mixer.current?.update(delta));
 
   return <primitive object={scene} ref={group} />;
 };
 
 function App() {
+  const [modelPath, setModelPath] = useState('/squat.glb');
+  const exercises = [
+    'squat.glb',
+    'sit_up.glb',
+    'side_lateral_raise.glb',
+    'pushup.glb',
+    'leg_raise.glb',
+    'dumbbell_tricep_extension.glb',
+    'dumbbell_shoulder_press.glb',
+    'dumbbell_fly.glb',
+    'dumbbelcurl.glb'
+  ];
+
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <Canvas>
         <Suspense fallback={null}>
-          <Model modelPath="/squat.glb" />
+          <Model modelPath={modelPath} />
           <Environment preset="sunset" />
           <OrbitControls />
         </Suspense>
       </Canvas>
+      <div style={{ position: 'absolute', top: 0, width: '100%', display: 'flex', justifyContent: 'center' }}>
+        {exercises.map(exercise => (
+          <button key={exercise} style={{ margin: '10px' }} onClick={() => setModelPath(`/${exercise}`)}>
+            {exercise.replace('.glb', '').replace('_', ' ')}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
